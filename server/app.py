4e609902a930a1db36e580f5fead52ac378c0fa6
BenWowo <111255005+BenWowo@users.noreply.github.com>
2023-10-22 00:06:04 -0500

    make app functionl

--- server/app.py
+++ server/app.py
@@ -1,22 +1,40 @@
-from flask import Flask
-from flask_migrate import Migrate
-from routes.blueprint import blueprint
-from models.machine import db
+# from flask import Flask
+# from flask_migrate import Migrate
+# from routes.blueprint import blueprint
+# from models.machine import db
+from flask import Flask, jsonify, request
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
+username = os.getenv("USER_MONGODB")
+password = os.getenv("PASS_MONGODB")
+mongo_uri = uri = f"mongodb+srv://{username}:{password}@scan-back-cluster.eoldn4r.mongodb.net/?retryWrites=true&w=majority"
 
 def create_app():
     app = Flask(__name__) 
     # app.config.from_object('config')  # Configuring from Python Files
-
-    db.init_app(app)
     return app
 
-
 app = create_app()
-# Registering the blueprint
-app.register_blueprint(blueprint)
-# migrate = Migrate(app, db)  # Initializing the migration
+client = MongoClient(uri, server_api=ServerApi('1'))
 
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    data = request.get_json()
+    base64_image = data.get('image_data')
+
+    # process the photo
+    # do business logic with the photo (machine learning part)
+    # and database database things
+
+    # list of savings that can be applied to receipt
+    savings = {"This is a dictionary that contains the savnigs": "These are the savings that a user will have"}
+    return jsonify(savings)
+
+# app.register_blueprint(blueprint)
+# migrate = Migrate(app, db)  # Initializing the migration
 
 if __name__ == '__main__':
     app.run()
