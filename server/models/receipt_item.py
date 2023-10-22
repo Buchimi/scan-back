e06fab2e671fd658b22834ed66023d4191cf7245
Buchimi <stormzzy900@gmail.com>
2023-10-22 02:45:35 -0500

    receipt_model

--- server/models/receipt_item.py
+++ server/models/receipt_item.py
@@ -1,10 +1,13 @@
-from dataclasses import dataclass
+# from dataclasses import dataclass
+from flask import Flask
+from mongoengine import Document, StringField, IntField, FloatField
+app = Flask(__name__)
 
-@dataclass
-class recept_item:
-    name: str
-    id: int
-    price: float
+# @dataclass
+class recept_item(Document):
+    name = StringField()
+    id = IntField(primary_key=True)
+    price= FloatField()
     
 # pydantic json into class
 # benie 
